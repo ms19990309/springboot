239779493c957eaeef89fa3a8df166782a92e5bf
mashuai <13935826473@163.com>
2020-12-01 18:45:39 +0800

    V1.0

---
+++ src/main/java/com/example/springboot_day3/SpringbootDay3Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot_day3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDay3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDay3Application.class, args);
+    }
+
+}
